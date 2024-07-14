40520bd498fe23772a18526bcf785dae2a8467ee
wenderson <ds.wendersonmelo@gmail.com>
2024-07-14 20:09:05 -0300

    UPDATE: Adicionado rotas de publicações e as assinaturas dos controllers

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicação adiciona uma nova publicação no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacoes retorna as publicações que aparecem no feed do usuário
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacao retorna apenas uma publicacao
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// AtualizarPublicacao edita as informações de uma publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletarPublicacao remove uma publicação do banco de dados
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ api/src/router/rotas/publicacoes.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasPublicacoes = []Rota{
+	{
+		Uri:                "/publicacoes",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacoes,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarPublicacao,
+		RequerAutenticacao: true,
+	},
+	{
+		Uri:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarPublicacao,
+		RequerAutenticacao: true,
+	},
+}
