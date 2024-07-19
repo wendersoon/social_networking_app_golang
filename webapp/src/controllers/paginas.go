5c115141b427cf9d217cc9c8c144eb5bee40514f
wenderson <ds.wendersonmelo@gmail.com>
2024-07-19 15:24:32 -0300

    UPDATE: CarregarPaginaPrincipal concluída

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,7 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/modelos"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -17,5 +23,27 @@ func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 
 // CarregarPaginaPrincipal carrega a pagina principal com as publicações
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
-	utils.ExecutarTemplate(w, "home.html", nil)
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+
+	response, err := requisicoes.FazerRequisicaoComAutenticacao(
+		r, http.MethodGet, url, nil,
+	)
+	if err != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: err.Error()})
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+	if err := json.NewDecoder(response.Body).Decode(&publicacoes); err != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: err.Error()})
+		return
+	}
+
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
