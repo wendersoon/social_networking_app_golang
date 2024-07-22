9082fbeeeef04165d9762793e71dc2b21252e028
wenderson <ds.wendersonmelo@gmail.com>
2024-07-22 17:16:54 -0300

    FIX: implementação de melhoria na rota de logout; UPDATE: adicionado redirecionamento na tela de login para usuários logados

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -17,6 +17,12 @@ import (
 
 // CarregarTelaDeLogin vai renderizar a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	cookie, _ := cookies.Ler(r)
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", http.StatusFound)
+		return
+
+	}
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
